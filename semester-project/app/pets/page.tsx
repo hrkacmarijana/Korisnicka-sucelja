084da3b8d94de6ed4f78de94d11bbba59da2ada5
Marijana Hrkac <mhrkac00@fesb.hr>
2023-10-19 13:46:34 +0200

    vjezba-1

---
+++ semester-project/app/pets/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Pets() {
+  return <h1 className="text-4xl flex justify-center">Pets</h1>;
+}
+
+export default Pets;
---
+++ semester-project/app/resources/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Resources() {
+  return <h1 className="text-4xl flex justify-center">Resources</h1>;
+}
+
+export default Resources;
