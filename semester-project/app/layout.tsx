1696aefe3b825401e6a10ab80ca7c0a8995159ea
Marijana Hrkac <mhrkac00@fesb.hr>
2023-10-19 13:59:13 +0200

    Ispravak-greske

--- semester-project/app/layout.tsx
+++ semester-project/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
-import { Menu } from "@headlessui/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
